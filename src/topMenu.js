3aad6b53166708d73cac3a3a2ecca9c594e6d310
Androkless <NO01VU@student.aau.dk>
2023-10-02 19:46:20 +0200

    Maaske virker det

---
+++ src/topMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TopMenu() {
+  return (
+    <div className="top-menu">
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default TopMenu
