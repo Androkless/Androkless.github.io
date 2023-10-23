583fc471d2b10c6b1337b05d5264d01bf3353676
Androkless <NO01VU@student.aau.dk>
2023-10-23 08:28:24 +0200

    hh

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./styles.css"
-import TopMenu from "./topMenu"
-
-export default function App() {
-  return <div>
-  <TopMenu />
-</div>
-}
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import DrawerAppBar from './components/drawerAppBar';
+
+export default function App() {
+  return (
+  <DrawerAppBar />
+  )
+}
